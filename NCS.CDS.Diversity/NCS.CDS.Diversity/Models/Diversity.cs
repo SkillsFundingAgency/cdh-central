ac91685bb53eb17092e71a350937de32f82e66a9
Kailash Bains <kailash.bains@ksbtechnologies.co.uk>
2018-06-01 13:29:11 +0100

    Initial Checkin For Diversity Microservice

---
+++ NCS.CDS.Diversity/NCS.CDS.Diversity/Models/Diversity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NCS.CDS.Diversity.Models
+{
+    public class Diversity
+    {
+        public Guid DiversityId { get; set; }
+
+        [Required]
+        public Guid CustomerId { get; set; }
+
+        [Required]
+        public int LLDDHealthProblemDeclarationId { get; set; }
+
+        [Required]
+        public int PrimaryLLDDHeathProblemId { get; set; }
+
+        [Required]
+        public int SecondaryLLDDHeathProblemId { get; set; }
+
+        [Required]
+        public Guid EthnicityId { get; set; }
+
+        [Required]
+        public Guid GenderId { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime DateCollected { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime LastModifiedDate { get; set; }
+
+        public Guid LastModifiedBy { get; set; }
+    } 
+}
