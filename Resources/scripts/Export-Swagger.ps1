49c81962cb79f5d99f647a5799945089fbea5cac
Nick Graham101 <nick.graham101@googlemail.com>
2018-06-06 11:14:43 +0100

    Removed verbose logging from export-swagger

--- Resources/scripts/Export-Swagger.ps1
+++ Resources/scripts/Export-Swagger.ps1
@@ -8,10 +8,9 @@ param(
 
 Import-Module $ModulePath
 $Swagger = Get-Swagger -FunctionAppName $FunctionAppName -ResourceGroup $ResourceGroup
-Write-Verbose $($Swagger | ConvertTo-Json)
+Write-Verbose -Message $($Swagger | ConvertTo-Json)
 $FileName = "$($FunctionAppName)_swagger-def_$([DateTime]::Now.ToString("yyyyMMdd-hhmmss"))"
-Write-Verbose "Filename: $FileName"
+Write-Verbose -Message "Filename: $FileName"
 $OutputFile = New-Item -Path $OutputFilePath -Name $FileName
 Set-Content -Path $OutputFile.FullName -Value $Swagger
 Write-Host "##[task.setvariable variable=SwaggerFile;isSecret=false;isOutput=true;]$FileName"
-Write-Verbose $env:SwaggerFile
