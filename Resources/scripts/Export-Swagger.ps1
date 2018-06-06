cd992e6fa98ffc355a32d525473594d577449759
Nick Graham101 <nick.graham101@googlemail.com>
2018-06-06 17:16:19 +0100

    Removed data stamp from swagger file name

--- Resources/scripts/Export-Swagger.ps1
+++ Resources/scripts/Export-Swagger.ps1
@@ -9,7 +9,8 @@ param(
 Import-Module $ModulePath
 $Swagger = Get-Swagger -FunctionAppName $FunctionAppName -ResourceGroup $ResourceGroup
 Write-Verbose -Message $($Swagger | ConvertTo-Json)
-$FileName = "$($FunctionAppName)_swagger-def_$([DateTime]::Now.ToString("yyyyMMdd-hhmmss"))"
+#$FileName = "$($FunctionAppName)_swagger-def_$([DateTime]::Now.ToString("yyyyMMdd-hhmmss")).json"
+$FileName = "$($FunctionAppName)_swagger-def.json"
 Write-Verbose -Message "Filename: $FileName"
 $OutputFile = New-Item -Path $OutputFilePath -Name $FileName
 Set-Content -Path $OutputFile.FullName -Value $Swagger
